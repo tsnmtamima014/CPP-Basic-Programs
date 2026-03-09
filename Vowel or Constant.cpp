ffdd1fd42129271c6f810f7e4b07e9fe9bb43e85
tsnmtamima014 <tsnmtamima@gmail.com>
2026-03-09 23:04:52 +0600

    Vowel or Constant

---
+++ Vowel or Constant.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    char c;
+     cout << "Enter an alphabet: ";
+    cin >> c;
+    if (c=='a'||c=='e'||c=='i'||
+        c=='o'||c=='u'||c=='A'||C=='E'||
+        c=='I'||C=='O'||C=='U'||)
+            cout << "Vowel";
+    else cout << "Constant";
+    return 0;
+}
