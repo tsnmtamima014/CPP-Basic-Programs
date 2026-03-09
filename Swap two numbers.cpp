bca7a044012ab1905b2ab848e36528c2fb50a1bd
tsnmtamima014 <tsnmtamima@gmail.com>
2026-03-09 21:00:14 +0600

    Swap two numbers

---
+++ Swap two numbers.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+int a = 10, b = 20, temp;
+temp = a; a = b; b = temp;
+cout << "a: " << a << ", b:" << b;
+return 0;
+}
--- Two floating point numbers.cpp
+++ Two floating point numbers.cpp
@@ -1,1 +1,10 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+float num1, num2, product;
+cout << "Enter two floating-point numbers:";
+cin >> num1 >> num2;
+product = num1 * num2;
+cout << "Product = " << product << endl;
+return 0;
+}
