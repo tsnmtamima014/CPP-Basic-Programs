f9bb39b9a54c2593c20b91d2436466ec6e324988
tsnmtamima014 <tsnmtamima@gmail.com>
2026-03-09 21:29:12 +0600

    int, float, double, char

--- Swap two numbers.cpp
+++ Swap two numbers.cpp
@@ -1,8 +1,1 @@
-#include <iostream>
-using namespace std;
-int main() {
-int a = 10, b = 20, temp;
-temp = a; a = b; b = temp;
-cout << "a: " << a << ", b:" << b;
-return 0;
-}
+
---
+++ int, float, double, char.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+cout << "int: " << sizeof(int) <<
+"bytes" << endl;
+cout << "float: " << sizeof(float) <<
+"bytes" << endl;
+cout << "double: " << sizeof(double) <<
+"bytes" << endl;
+cout << "char: " << sizeof(char) <<
+"bytes" << endl;
+return 0;
+}
