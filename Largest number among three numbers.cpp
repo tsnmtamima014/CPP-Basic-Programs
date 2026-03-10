ba2b65800ed9e0bcaad3213f9043076d3841a790
tsnmtamima014 <tsnmtamima@gmail.com>
2026-03-10 11:09:44 +0600

    Calculate sum of natural numbers

--- Largest number among three numbers.cpp
+++ Largest number among three numbers.cpp
@@ -1,17 +1,1 @@
-#include <iostream>
-using namespace std;
-int main() {
-    float n1, n2, n3;
-    cout << "Enter three numbers: ";
-    cin >> n1 >> n2 >> n3;
-    if(n1 >= n2 && n1 >= n3) {
-            cout << "The largest number is: " << n1 << endl;
-    }
-    else if(n2 >= n1 && n2 >= n3) {
-            cout << "The largest number is: " << n2 << endl;
-    }
-    else {
-            cout << "The largest number is: " << n3 << endl;
-    }
-    return 0;
-}
+
