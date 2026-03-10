79ac523587c93129bfca24b924b8e26a40762ff4
tsnmtamima014 <tsnmtamima@gmail.com>
2026-03-10 11:34:27 +0600

    Alphabets A to Z

---
+++ Alphabets A to Z.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    char c;
+    cout << "Alphabets from A to Z are: " << endl;
+    for (c = 'A'; c <= 'Z'; ++c) {
+            cout << c << " ";
+    }
+cout << endl;
+    return 0;
+    }
