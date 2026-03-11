ffdc400ecfea5a9c0250b5386854a965876f5d52
tsnmtamima014 <tsnmtamima@gmail.com>
2026-03-11 16:30:06 +0600

    Elements in an array

---
+++ Elements in an array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <map>
+int main() {
+    int n, x;
+    std::map<int, int> f;
+    std::cin >> n;
+    while(n--) { std::cin >> x; f[x]+
+        +;
+        }
+        for(auto const& [v, c] : f)
+            std::cout << v << " : " << c << " ";
+        return 0;
+}
+
