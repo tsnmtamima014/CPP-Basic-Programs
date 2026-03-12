ed392ecf9495e5adbb7a6fc444bad21e5ea02356
tsnmtamima014 <tsnmtamima@gmail.com>
2026-03-12 20:23:36 +0600

    Full diamond shape

--- Diamond shape.cpp
+++ Diamond shape.cpp
@@ -1,1 +1,12 @@
 
+#include <iostream>
+using namespace std;
+int main() {
+    int n = 5;
+    for (int i = 1; i < 2 * n; i++) {
+        int r = (i <= n) ? i : 2 * n - i;
+        for (int j = 1; j < 2 * n; j++)
+            cout << (j == n-r+1 || j == n+r-1 ? "*" : " ");
+        cout << "\n";
+    }
+}
