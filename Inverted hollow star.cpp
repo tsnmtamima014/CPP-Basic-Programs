4468e3b9893acfee5fa0c8a4b4953a98429d3601
tsnmtamima014 <tsnmtamima@gmail.com>
2026-03-12 20:18:02 +0600

    Inverted hollow star

---
+++ Inverted hollow star.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n = 5;
+    for (int i = n; i >= 1; i--) {
+            for (int j = 1; j < 2 * n; j++)
+            cout << (j == n -i+1 || j == n +i-1 || i == n ? "*" : " ");
+    cout << "\n";
+    }
+    return 0;
+}
