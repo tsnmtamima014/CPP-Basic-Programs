1607434803e8d4fc5515e9b0be12d8333b493b92
tsnmtamima014 <tsnmtamima@gmail.com>
2026-03-12 20:27:02 +0600

    Pascal,s triangle

--- Full diamond shape.cpp
+++ Full diamond shape.cpp
@@ -1,1 +1,12 @@
 
+#include <iostream>
+using namespace std;
+int main() {
+    int n = 5;
+    for (int i = 1; i < 2 * n; i++) {
+        int r = (i <= n) ? i : 2 * n - i;
+        for (int j = 1; j <= n - r; j++) cout << " ";
+        for (int j = 1; j < 2 * r; j++) cout << "*";
+        cout << "\n";
+    }
+}
