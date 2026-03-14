1b8990d75074041621e1af9cc5799047668e45d6
tsnmtamima014 <tsnmtamima@gmail.com>
2026-03-14 20:56:32 +0600

    Lexicographically

--- Compare two string.cpp
+++ Compare two string.cpp
@@ -1,1 +1,9 @@
-
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string s1 = "Apple", s2 = "Apple";
+    if (s1 == s2) cout << "Strings are equal";
+    else cout << "Strings are not equal";
+    return 0;
+}
