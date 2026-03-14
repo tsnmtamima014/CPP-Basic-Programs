f99a530474f3e1b8af4a100aca33834ac956aba0
tsnmtamima014 <tsnmtamima@gmail.com>
2026-03-14 17:56:45 +0600

    Two strings

---
+++ Two strings.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string s1 = "Hello ", s2 = "World!";
+    string res = s1 + s2;
+    cout << "Concatenated: " << res << endl;
+    return 0;
+}
